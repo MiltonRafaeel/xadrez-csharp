c2e292688fb843e9edcb948d6e05ef86715b55c4
MiltonRafaeel <miltonrafael166@gmail.com>
2026-09-04 21:45:17 -0400

    Prepare to moves

--- Program.cs
+++ Program.cs
@@ -7,19 +7,30 @@ namespace projeto
     {
         static void Main(string[] args)
         {
+
             try
             {
-                Tabuleiro tab = new Tabuleiro(8, 8);
+                PartidaDeXadrez partida = new PartidaDeXadrez();
 
-                tab.colocarPeca(new Torre(tab, Cor.Amarela), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Amarela), new Posicao(1, 3));
-                tab.colocarPeca(new Rei(tab, Cor.Amarela), new Posicao(0, 9));
+                while (!partida.terminada)
+                {
 
-                Tela.imprimirTabuleiro(tab);
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab);
+
+                    Console.WriteLine();
+                    Console.Write("Origem: ");
+                    Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+                    Console.Write("Destino: ");
+                    Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+
+                    partida.executaMovimento(origem, destino);
+                }
+
             }
             catch (TabuleiroException e)
             {
-                System.Console.WriteLine(e.Message);
+                Console.WriteLine(e.Message);
             }
 
         }
