6ea58e185a0f12bee2487cd6509f74c5be0de813
MiltonRafaeel <miltonrafael166@gmail.com>
2026-09-04 20:11:37 -0400

    Exception custom

--- tabuleiro/Tabuleiro.cs
+++ tabuleiro/Tabuleiro.cs
@@ -18,10 +18,35 @@ namespace tabuleiro
             return pecas[linha, coluna];
         }
 
+        public Peca peca(Posicao pos)
+        {
+            return pecas[pos.Linha, pos.Coluna];
+        }
+
+        public bool existePeca(Posicao pos)
+        {
+            validarPosicao(pos);
+            return peca(pos) != null;
+        }
+
         public void colocarPeca(Peca p, Posicao pos)
         {
+            if (existePeca(pos)) throw new TabuleiroException("Ja existe uma peca na mesma posicao !");
             pecas[pos.Linha, pos.Coluna] = p;
             p.posicao = pos; 
+        }
+
+        public bool posicaoValida(Posicao pos)
+        {
+            return pos.Linha < 0 || pos.Linha >= Linhas || pos.Coluna < 0 || pos.Coluna >= Colunas ? false : true;
+        }
+
+        public void validarPosicao(Posicao pos)
+        {
+            if (!posicaoValida(pos))
+            {
+                throw new TabuleiroException("Posicao invalida");
+            }
         }
     }
 }
